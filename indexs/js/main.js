7a26b4a081d18701eca9e2add2b54c0dd27a0b2b
lft <752470614@qq.com>
2019-03-31 19:45:48 +0800

    是U

--- indexs/js/main.js
+++ indexs/js/main.js
@@ -2,12 +2,6 @@ function maxwidth() {
   // if ($(window).width() > 700) {
   //   window.location.href = "indextow.html";
   // }
-  if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-    alert('手机端')
-  } else {
-    alert('PC端')
-  }
-
 }
 
 (function() {
